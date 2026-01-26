b21a4c40d54c017cbe47271bc8a615bee52df246
09samuel <samuelfernandes009@gmail.com>
2026-01-26 13:41:38 +0530

    added logging

--- workers/backup_db.worker.js
+++ workers/backup_db.worker.js
@@ -3,12 +3,23 @@ const {handleBackupDBJob} = require("../handlers/handleBackupDBJob");
 
 console.log("Backup worker started");
 
-new Worker(
+const worker = new Worker(
   "backup-db",
   async (job) => {
+    console.log("JOB RECEIVED BY WORKER", job.id, job.name);
     await handleBackupDBJob(job);
   },
   {
     connection: { host: "localhost", port: 6379 },
   }
 );
+
+worker.on("failed", (job, err) => {
+  console.error("JOB FAILED", job?.id, err);
+});
+
+worker.on("error", (err) => {
+  console.error("WORKER ERROR", err);
+});
+
+
