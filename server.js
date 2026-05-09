7b9b19c5e57e14d4a14af76bec23e94d3b8384a4
09samuel <samuelfernandes009@gmail.com>
2026-05-09 22:31:03 +0530

    update docker build w no cache

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors({
 //health check endpoint
 app.get('/health', (req, res) => {
   res.status(200).json({
-    status: 'OK',
+    status: 'UP',
     timestamp: new Date().toISOString()
   });
 });
